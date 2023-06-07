18b6ed4f51b10ed9d084ea64b9af658a1e6614b9
Baran Günay <baranngunay@gmail.com>
2023-06-07 15:43:24 +0300

    Dosyalama Düzeni Düzeltme
    Dosyaların dallanmasındaki hata düzeltildi

--- App.Web.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_PROJE.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
